6ea604f398ed8313950bf5b6154bcdc5e5c2e53d
svn <svn@c42ff92a-1dd5-4453-a422-b00c20660b90>
2014-06-26 13:41:45 +0000

    started C# in trunk

---
+++ trunk/model/Axon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odin.model
+{
+    class Axon
+    {
+        Neuron fromNeuron;
+        List<Synapse> synapses;
+        internal Axon(Neuron neuron)
+        {
+            fromNeuron = neuron;
+        }
+    }
+}
---
+++ trunk/model/Neuron.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odin.model
+{
+    class Neuron
+    {
+        Axon axon;
+
+        List<Dendrite> dendrites;
+        internal Neuron()
+        {
+            axon = new Axon(this);
+            
+        }
+        internal Dendrite getNewDendrite() {
+            Dendrite tmp = new Dendrite(this);
+            dendrites.Add(tmp);
+            return tmp;
+        }
+
+    }
+}
