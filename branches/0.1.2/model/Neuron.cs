9c2a69fb27aa2a26e079ef20991e177e51129269
svn <svn@c42ff92a-1dd5-4453-a422-b00c20660b90>
2014-07-16 13:06:39 +0000

    first working version based on c#

---
+++ branches/0.1.2/model/Neuron.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odin.model
+{
+    class Neuron
+    {
+        Axon axon;
+        Brain brain;
+        internal double activation = 0;
+        internal UInt64 lastFired = 0;
+        internal int layer;
+        internal int tag;
+
+
+        List<Dendrite> dendrites = new List<Dendrite>();
+        internal Neuron(Brain mybrain)
+        {
+            this.id = mybrain.getNextID();
+            axon = new Axon(this,mybrain);
+            brain = mybrain;
+            this.layer = 1;
+            this.tag = 0;
+
+        }
+        internal Dendrite getNewDendrite(int length) {
+            Dendrite tmp = new Dendrite(this,brain,length);
+            dendrites.Add(tmp);
+            return tmp;
+        }
+        internal Dendrite getDendrite(int length)
+        {
+            if (this.dendrites.Exists(l => l.length == length))
+            {
+                return this.dendrites.Find(l => l.length == length);
+            }
+            else
+            {
+                return getNewDendrite(length);
+            }
+        }
+        internal void fire()
+        {
+            brain.log("Fired: " + this.id);
+            brain.monitorOutput(this.tag);
+            this.activation = 0;
+            this.lastFired = brain.currentStep;
+            brain.addToRecoveryQueue(this);
+            brain.lateralInhibition(this.layer);
+            this.axon.propagateActionPotential();
+        }
+
+
+        internal void synapseOn(Dendrite tmpDendrite)
+        {
+            
+            this.axon.synapseOn(tmpDendrite);
+            
+        }
+
+        internal bool crossesThreshold()
+        {
+            if (this.activation >= brain.activationThreshold) {
+                return true;
+            }
+            return false;
+        }
+
+        public int id { get; set; }
+
+        internal List<Neuron> getSuccessors()
+        {
+            return this.axon.getSuccessors();
+        }
+
+        internal void polarize(double p)
+        {
+            this.activation += p;
+            brain.log("Polarized: " + this.id + " with Activation: " + p + " now having " + this.activation);
+            
+        }
+
+        internal List<Neuron> getPredecessors()
+        {
+            List<Neuron> neurons = new List<Neuron>();
+            foreach (Dendrite d in this.dendrites)
+            {
+                foreach (Neuron n in d.getPredecessors())
+                {
+                    neurons.Add(n);
+                }
+            }
+            return neurons;
+        }
+
+        internal List<Dendrite> getDendrites()
+        {
+            return (this.dendrites);
+        }
+
+        internal void leakActivation()
+        {
+            this.activation *= Math.Pow(brain.leakageFactor, (brain.currentStep - this.lastFired));
+        }
+
+        internal void inhibit()
+        {
+            this.activation = 0;//*= 0.5;
+        }
+    }
+}
---
+++ branches/0.1.2/model/Synapse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odin.model
+{
+    class Synapse
+    {
+        Axon fromAxon;
+        Dendrite toDendrite;
+        Brain brain;
+        internal int countExcitatorySynapses;
+        internal int countInhibitorySynapses = 0;
+
+        internal Synapse(Axon axon, Dendrite dendrite, Brain mybrain) {
+            this.brain = mybrain;
+            this.fromAxon = axon;
+            this.toDendrite = dendrite;
+            this.countExcitatorySynapses = brain.synapseDefaultCount;
+            //toDendrite.addSynapse(this);
+        }
+        internal Axon getFromAxon()
+        {
+            return this.fromAxon;
+        }
+        internal void activate()
+        {
+            toDendrite.activate(this.getStrength());
+        }
+
+        internal Neuron getPredecessor()
+        {
+            Axon a = this.getFromAxon();
+            return (a.getParentNeuron());
+        }
+
+        internal void reinforce(bool excitatory)
+        {
+            if (excitatory) this.countExcitatorySynapses++;
+            else this.countInhibitorySynapses++;
+        }
+        internal double getStrength() {
+            return brain.synapseDefaultStrength * (this.countExcitatorySynapses - this.countInhibitorySynapses);
+        }
+
+        internal Neuron getNeuron()
+        {
+            return (this.toDendrite.getNeuron());
+        }
+    }
+}
