e38033eb3bd1df88f7bf57e2e3f741e27e9e93bf
svn <svn@c42ff92a-1dd5-4453-a422-b00c20660b90>
2014-12-11 14:46:56 +0000

    first working db version

---
+++ branches/odin db 0.1.2/model/Dendrite.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odin.model
+{
+    class Dendrite
+    {
+        
+        List<Synapse> synapses = new List<Synapse>();
+        internal int length;
+        
+        
+        Neuron toNeuron;
+        Brain brain;
+        
+        internal Dendrite(Neuron neuron, Brain mybrain, int len)
+        {
+            toNeuron = neuron;
+            brain = mybrain;
+            this.length = len;
+            
+            
+        }
+
+        internal void activate(double activationVal)
+        {
+            brain.activationQueue.addToStep(toNeuron,length,activationVal);
+        }
+
+
+        internal Synapse addSynapse(Axon axon)
+        {
+            Synapse s = new Synapse(axon,this,brain);
+          
+            this.synapses.Add(s);
+            return s;
+        }
+
+        internal Neuron getNeuron()
+        {
+            return this.toNeuron;
+        }
+
+        internal List<Neuron> getPredecessors()
+        {
+            List<Neuron> neurons = new List<Neuron>();
+            foreach (Synapse s in this.synapses)
+            {
+                neurons.Add(s.getPredecessor());
+            }
+            return neurons;
+        }
+
+        internal IEnumerable<Synapse> getSynapses()
+        {
+            return this.synapses;
+        }
+
+        internal Synapse getSynapse(Axon axon)
+        {
+            foreach (Synapse s in this.synapses)
+            {
+                if (s.getFromAxon() == axon) return s;
+
+            }
+            return this.addSynapse(axon);
+        }
+
+        internal double countSynapses()
+        {
+            return (this.synapses.Count);
+        }
+    }
+}
---
+++ branches/odin db 0.1.2/model/Synapse.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odin.model
+{
+    class Synapse
+    {
+        Axon fromAxon;
+        Dendrite toDendrite;
+        Brain brain;
+        internal int countExcitatorySynapses;
+        internal int countInhibitorySynapses = 0;
+        internal double weight;
+
+        internal Synapse(Axon axon, Dendrite dendrite, Brain mybrain) {
+            this.brain = mybrain;
+            this.fromAxon = axon;
+            this.toDendrite = dendrite;
+            this.countExcitatorySynapses = brain.synapseDefaultCount;
+            this.weight = brain.synapseDefaultStrength;
+            //toDendrite.addSynapse(this);
+        }
+        internal Axon getFromAxon()
+        {
+            return this.fromAxon;
+        }
+        
+
+        internal Neuron getPredecessor()
+        {
+            Axon a = this.getFromAxon();
+            return (a.getParentNeuron());
+        }
+
+        internal void reinforce(bool excitatory)
+        {
+            if (excitatory && (this.countExcitatorySynapses-this.countInhibitorySynapses) <= brain.synapseMaxCount)
+            {
+                this.countExcitatorySynapses++;
+                //this algorithm is taken from James A. Anderson, 'Introduction to Neural Networks' (MIT Press)
+                //related to Kohonen's SOFM.
+                this.weight = this.weight - (this.weight - 1) *  brain.adaptionRate / (this.countExcitatorySynapses+this.countInhibitorySynapses);
+            }
+            else if (!excitatory && (this.countInhibitorySynapses - this.countExcitatorySynapses) <= brain.synapseMaxCount)
+            {
+                this.countInhibitorySynapses++;
+                this.weight = this.weight * (1 - brain.adaptionRate / (this.countExcitatorySynapses + this.countInhibitorySynapses));
+            }
+        }
+        internal double getStrength() {
+            return this.weight;
+        }
+
+        internal Neuron getNeuron()
+        {
+            return (this.toDendrite.getNeuron());
+        }
+
+        internal void activate(int p)
+        {
+            if (brain.activateNeuronBasedOnInputSynapses)
+            {
+                toDendrite.activate(this.getStrength() / p / toDendrite.countSynapses());
+            }
+            else
+            {
+                toDendrite.activate(this.getStrength() / p);
+            }
+            if (brain.learnOnActivate) this.reinforce(true);
+        }
+        internal void activate()
+        {
+            this.activate(1);
+        }
+    }
+}
